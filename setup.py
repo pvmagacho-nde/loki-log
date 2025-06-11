1f5ca33a3844a76bb3888aef5fab264d16af3ec4
Paulo Vitor Magacho <paulo@newdebtexchange.com>
2025-06-11 09:31:39 -0300

    add specific methods for levels. push version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="loki-log",
-    version="0.1.0",
+    version="0.1.1",
     author="Paulo Vitor Magacho",
     author_email="paulo@nde.com",
     description="A simple structured Loki compatible logging utility for Python",
